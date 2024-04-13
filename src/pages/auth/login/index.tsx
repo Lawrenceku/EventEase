09b46ee2a8ca7ab7bbece69b2a56426b09c6964d
deepeegee <tosinorunesajo@gmail.com>
2024-04-13 21:21:00 +0100

    login img

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -36,77 +36,81 @@ export default function SignIn() {
   });
 
   return (
-    <div
-      className={cn(
-        "flex h-screen items-center justify-center",
+    <div className="flex h-screen">
+      <div className="hidden md:block w-1/2">
+        <img src="img.jpg" alt="image" className="h-full w-full object-cover" />
+      </div>
+      <div className={cn(
+        "flex justify-center items-center",
+        "w-full md:w-1/2",
         openSans.className,
-      )}
-    >
-      <div className="mx-6 w-full space-y-5 sm:w-[20rem] xl:w-[30rem] 2xl:w-[40rem]">
-        <div className="space-y-5 text-center">
-          <h1 className="text-3xl font-bold">Sign In</h1>
-          <p>
-            Don&apos;t have an account?{" "}
-            <Link
-              href="/auth/register"
-              className="hover:text-[#0028e5 text-primary hover:text-secondary"
-            >
-              Sign Up
-            </Link>
-          </p>
-        </div>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
-            <FormInput
-              control={form.control}
-              name="email"
-              type="email"
-              placeholder="Email"
-              className="text-md h-12 rounded-lg px-6"
-            />
-            <FormInput
-              control={form.control}
-              name="password"
-              type="password"
-              placeholder="Password"
-              className="text-md h-12 rounded-lg px-6"
-            />
-            <Button
-              disabled={loading}
-              type="submit"
-              className="h-12 w-full gap-2 text-lg"
-            >
-              Sign In
-              {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-            </Button>
-          </form>
-        </Form>
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
+      )}>
+        <div className="mx-6 w-full space-y-5 sm:w-[20rem] xl:w-[30rem] 2xl:w-[40rem]">
+          <div className="space-y-5 text-center">
+            <h1 className="text-3xl font-bold">Sign In</h1>
+            <p>
+              Don&apos;t have an account?{" "}
+              <Link
+                href="/auth/register"
+                className="hover:text-[#0028e5 text-primary hover:text-secondary"
+              >
+                Sign Up
+              </Link>
+            </p>
           </div>
-          <div className="relative flex justify-center text-xs">
-            <span className="bg-background px-5 text-muted-foreground">or</span>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+              <FormInput
+                control={form.control}
+                name="email"
+                type="email"
+                placeholder="Email"
+                className="text-md h-12 rounded-lg px-6"
+              />
+              <FormInput
+                control={form.control}
+                name="password"
+                type="password"
+                placeholder="Password"
+                className="text-md h-12 rounded-lg px-6"
+              />
+              <Button
+                disabled={loading}
+                type="submit"
+                className="h-12 w-full gap-2 text-lg"
+              >
+                Sign In
+                {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+              </Button>
+            </form>
+          </Form>
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs">
+              <span className="bg-background px-5 text-muted-foreground">or</span>
+            </div>
           </div>
+          <Button
+            variant="outline"
+            type="button"
+            disabled={loading}
+            className="h-12 w-full gap-3"
+          >
+            <FcGoogle className="h-5 w-5" />
+            Sign In with Google
+          </Button>
+          <Button
+            variant="outline"
+            type="button"
+            disabled={loading}
+            className="h-12 w-full gap-3"
+          >
+            <FaApple className="h-5 w-5" />
+            Sign In with Apple
+          </Button>
         </div>
-        <Button
-          variant="outline"
-          type="button"
-          disabled={loading}
-          className="h-12 w-full gap-3"
-        >
-          <FcGoogle className="h-5 w-5" />
-          Sign In with Google
-        </Button>
-        <Button
-          variant="outline"
-          type="button"
-          disabled={loading}
-          className="h-12 w-full gap-3"
-        >
-          <FaApple className="h-5 w-5" />
-          Sign In with Apple
-        </Button>
       </div>
     </div>
   );
